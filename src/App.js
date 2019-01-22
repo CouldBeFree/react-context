6b4821d69f56481dc01dbfce56ac2aab58c45714
Explorer <propokerstar90@gmail.com>
2019-01-22 20:59:45 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//New context
+const MyContext = React.createContext();
+
+//Provider Component
+class MyProvider extends Component {
+    state = {
+        person: 'Wesley',
+        age: 23
+    };
+
+    render(){
+        return(
+            <MyContext.Provider value={{
+                state: this.state,
+                growOlder: () => {
+                  this.setState({
+                      age: this.state.age + 1
+                  })
+                }
+            }}>
+                {this.props.children}
+            </MyContext.Provider>
+        )
+    }
+}
+
+const Family = (props) => (
+    <div className='family'>
+        <Person/>
+    </div>
+);
+
+class Person extends Component{
+    render(){
+        return(
+            <div>
+                <MyContext.Consumer>
+                    {(context) => (
+                        <div>
+                            <p>My age is {context.state.age}</p>
+                            <p>My name is {context.state.person}</p>
+                            <button onClick={context.growOlder}>Click me</button>
+                        </div>
+                    )}
+                </MyContext.Consumer>
+            </div>
+        )
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MyProvider>
+                <div>
+                    <h1>I am the app</h1>
+                    <Family />
+                </div>
+            </MyProvider>
+        );
+    }
 }
 
 export default App;
